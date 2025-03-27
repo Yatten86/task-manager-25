38e3235673f6c8960caab881832d5a0ff8a51d86
Yatten86 <cosmoreddog@gmail.com>
2025-03-27 22:12:07 +0200

    Add delete and edit functionality for tasks

--- src/components/TaskList/TaskList.jsx
+++ src/components/TaskList/TaskList.jsx
@@ -2,11 +2,21 @@ import React from "react";
 import Task from "../Task/Task";
 import "./TaskList.css";
 
-export default function TaskList({ tasks }) {
+export default function TaskList({ tasks, onDelete, onEdit }) {
   return (
     <div className="task-list">
       {tasks.map((task, index) => (
-        <Task key={index} title={task.title} description={task.description} />
+        <Task
+          key={index}
+          title={task.title}
+          description={task.description}
+          onDelete={() => {
+            onDelete(index);
+          }}
+          onEdit={(newTitle, newDescription) =>
+            onEdit(index, newTitle, newDescription)
+          }
+        />
       ))}
     </div>
   );
