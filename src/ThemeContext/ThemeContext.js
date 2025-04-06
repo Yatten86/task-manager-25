7b583dcd4abd5953cdf109757d9222fd2d2d57e8
Yatten86 <cosmoreddog@gmail.com>
2025-04-07 00:13:14 +0300

    Add persistance for tasks using Local Storage

--- src/ThemeContext/ThemeContext.js
+++ src/ThemeContext/ThemeContext.js
@@ -1,12 +1,18 @@
 import React, { createContext, useState } from "react";
+import {
+  loadThemeFromLocalStorage,
+  saveThemeToLocalStorage,
+} from "../utils/localStorageUtils";
 
 export const ThemeContext = createContext();
 
 export const ThemeProvider = ({ children }) => {
-  const [theme, setTheme] = useState("light");
+  const [theme, setTheme] = useState(loadThemeFromLocalStorage()); //load saved theme from local storage
 
   const toggleTheme = () => {
-    setTheme((prevTheme) => (prevTheme === "light" ? "dark" : "light"));
+    const newTheme = theme === "light" ? "dark" : "light";
+    setTheme(newTheme);
+    saveThemeToLocalStorage(newTheme); // save selected theme to local storage
   };
 
   return (
