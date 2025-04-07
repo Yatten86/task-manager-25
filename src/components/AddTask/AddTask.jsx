b69633487f746cfa32d1b54b89019e3fda6fc7a2
Yatten86 <cosmoreddog@gmail.com>
2025-04-07 20:48:47 +0300

    Add priority feature for tasks

--- src/components/AddTask/AddTask.jsx
+++ src/components/AddTask/AddTask.jsx
@@ -4,11 +4,13 @@ import "./AddTask.css";
 export default function AddTask({ onAdd }) {
   const [title, setTitle] = useState("");
   const [description, setDescription] = useState("");
+  const [priority, setPriority] = useState("Medium"); //default priority
 
   const handleAdd = () => {
-    onAdd({ title, description });
+    onAdd({ title, description, priority });
     setTitle("");
     setDescription("");
+    setPriority("Medium"); //Reset field after adding the task
   };
 
   return (
@@ -25,6 +27,16 @@ export default function AddTask({ onAdd }) {
         value={description}
         onChange={(e) => setDescription(e.target.value)}
       />
+      <select
+        name="priority"
+        id="priority"
+        value={priority}
+        onChange={(e) => setPriority(e.target.value)}
+      >
+        <option value="High">High</option>
+        <option value="Medium">Medium</option>
+        <option value="Low">Low</option>
+      </select>
 
       <button onClick={handleAdd}>Add Task</button>
     </div>
